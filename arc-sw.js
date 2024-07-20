d4de5b93999eec01625090102206bc0820bb18de
Ridwan <59315983+ridwan12782@users.noreply.github.com>
2024-07-20 23:56:42 +0700

    Update arc-sw.js

--- arc-sw.js
+++ arc-sw.js
@@ -1,8 +1,17 @@
-// arc-sw.js
 self.addEventListener('install', (event) => {
     console.log('Service Worker installing.');
-});
-
-self.addEventListener('activate', (event) => {
+  });
+  
+  self.addEventListener('activate', (event) => {
     console.log('Service Worker activating.');
-});
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          return response || fetch(event.request);
+        })
+    );
+  });
+  
